ccf09146607dec32a65b68f62a2203b69a398aac
Vlad <vladlisin2@gmail.com>
2020-10-10 16:28:35 +0700

    bot

--- think_bank/views.py
+++ think_bank/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from django.http import StreamingHttpResponse, HttpResponseRedirect, HttpResponse
 
 # Create your views here.
 
 
+@csrf_exempt
 def Bot(request):
     if request.method == 'POST':
         print('INEEDHELP')
         return HttpResponse("dcf1fa33")
-
     print(request)
     print('IAMRETARDED')
     return HttpResponse("dcf1fa33")
