c60a9359267d50e1915526b678de9638341e7e0d
Vlad <vladlisin2@gmail.com>
2020-10-11 17:29:55 +0700

    bot

--- think_bank/views.py
+++ think_bank/views.py
@@ -14,7 +14,9 @@ def Bot(request):
         if (type == 'confirmation'):
             return HttpResponse("a6d0d81e")
         if (type == 'message_new'):
-            print(data)
+            user_id = data['user_id']
+            post = data['data']
+            print(user_id, post)
             return HttpResponse('Success')
         return HttpResponse("a6d0d81e")
     return HttpResponse("Wrong request")
