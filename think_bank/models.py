29f1c09a1be74555d1734499eb48cc1a4a8296d4
Vlad <vladlisin2@gmail.com>
2020-10-13 15:36:23 +0700

    bot

--- think_bank/models.py
+++ think_bank/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class VkUser(models.Model):
-    user_id = models.IntegerField(default=0)
+    user_id = models.BigIntegerField(default=0)
     user_img = models.CharField(default='Не указано', max_length=300)
     user_name = models.CharField(default='Не указано', max_length=300)
     token = models.CharField(default='', max_length=300)
@@ -14,17 +14,17 @@ class Post(models.Model):
     # владелец поста в банке креативов
     user = models.ForeignKey(
         VkUser, blank=False, null=False, related_name='vkuser_id', on_delete=models.CASCADE)
-    post_id = models.IntegerField(default=0)
-    owner_id = models.IntegerField(default=0)
+    post_id = models.BigIntegerField(default=0)
+    owner_id = models.BigIntegerField(default=0)
     owner_name = models.CharField(default='Не указано', max_length=1000)
     owner_img_link = models.CharField(default='Не указано', max_length=1000)
-    from_id = models.IntegerField(default=0)
+    from_id = models.BigIntegerField(default=0)
     date = models.CharField(default='Не указано', max_length=300)
     text = models.CharField(default='Не указано', max_length=30000)
-    comments_count = models.IntegerField(default=0)
-    likes_count = models.IntegerField(default=0)
-    reposts_count = models.IntegerField(default=0)
-    views_count = models.IntegerField(default=0)
+    comments_count = models.BigIntegerField(default=0)
+    likes_count = models.BigIntegerField(default=0)
+    reposts_count = models.BigIntegerField(default=0)
+    views_count = models.BigIntegerField(default=0)
     attachments = models.CharField(default='Не указано', max_length=30000)
 
 
