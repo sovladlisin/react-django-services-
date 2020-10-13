078af89d4d85f39aa56dfa7bae9927d841e4ae68
Vlad <vladlisin2@gmail.com>
2020-10-13 20:31:48 +0700

    bot

--- think_bank/api.py
+++ think_bank/api.py
@@ -202,13 +202,13 @@ def add_post_to_db(id_check, post_link, user_id, comment):
     else:
         owner = vk_request('get', 'users.get', {
                            'user_ids': wall_data['owner_id'], 'fields': 'photo_50'}, user.token, '5.124')['response'][0]
-        owner_name = owner['first_name'] + owner['last_name']
+        owner_name = owner['first_name'] + " " + owner['last_name']
         owner_photo = owner['photo_50']
 
     new_post = Post(user=user,
                     post_id=post_id,
                     owner_id=wall_data['owner_id'],
-                    owner_name=owner_name,
+                    owner_name=wall_data['id'],
                     owner_img_link=owner_photo,
                     from_id=wall_data['from_id'],
                     date=wall_data['date'],
